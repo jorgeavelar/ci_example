91ee81fea7adb88b37d886f1bb06e6d2891151b7
Jorge Avelar <jorgecavelar@gmail.com>
2022-02-22 12:16:12 -0300

    change ci configuration

--- tests/health_test.py
+++ tests/health_test.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from config.routes import app
 
 client = TestClient(app)
 
+
 def test_health_route_sucessfuly():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+
+def test_health_route_failure():
+    response = client.get("/foobar")
+    assert response.status_code == 400
+
+
+def test_with_wrong_test():
+    response = client.get("/foobar")
+    assert response.status_code == 200
